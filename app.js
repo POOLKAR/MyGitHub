1e66d18b6f057a58c59f76f813867417c3b5784e
Opilane TTHK <opilane@tthk.ee>
2019-10-14 09:31:40 +0300

    My GitHub Profile

---
+++ app.js
@@ -0,0 +1,59 @@
+//Init Github Class
+const github = new Github;
+//Init UI Class
+const ui = new UI;
+
+github.getUser("POOLKAR")
+        .then(data => {
+            if(data.profile.message === 'Not Found') {
+                ui.showAlert('User not found!', 'alert alert-danger')
+            } else {
+                ui.showProfile(data.profile);
+                ui.showRepos(data.repos);
+            }
+        })
+
+const searchUser = document.getElementById('searchUser');
+
+searchUser.addEventListener('keyup', (event) => {
+    //get the input text from the searchUser input field
+    const userText = event.target.value;
+    if(userText !== '') {
+        //make an http call to GitHub API
+        github.getUser(userText)
+        .then(data => {
+            if(data.profile.message === 'Not Found') {
+                //Show alert if user was not found
+                ui.showAlert('User Not Found', 'alert alert-danger');
+            } else {
+                //Show user profile and repos
+                ui.showProfile(data.profile);
+                ui.showRepos(data.repos);
+            }
+            console.log(data);
+        })
+    }else {
+            ui.clearProfile();
+        }
+})
+
+
+
+
+
+
+
+/*loadEventListeners();
+
+function loadEventListeners() {
+    document.addEventListener('DOMContentLoaded', getObject);
+}
+
+function getObject() {
+    const gitHub = new Github;
+
+    gitHub.getUser('ju-voro')
+    .then(data=> {
+        console.log(data);
+    });
+}*/
